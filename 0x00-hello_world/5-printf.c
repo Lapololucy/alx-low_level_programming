de0b41767d1bb4eb610fd6a9d082ff7b63af8216
Lapololucy <lapololucy@gmail.com>
2022-10-13 06:53:56 -0700

    Task.5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
