c013df9ae222e4edf6bfc1b37b35f1007acb77b1
Lapololucy <lapololucy@gmail.com>
2022-10-15 05:06:34 -0700

    Task.1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 != 0 && % 10 < 6)
+	if (n % 10 != 0 && n % 10 < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
