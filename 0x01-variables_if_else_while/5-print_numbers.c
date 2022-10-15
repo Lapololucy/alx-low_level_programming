84de8d23ab5cc59f5d0360aeaae49baacc23a9b0
Lapololucy <lapololucy@gmail.com>
2022-10-15 06:27:53 -0700

    Task.5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
+
