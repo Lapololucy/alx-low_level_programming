e292e9f0636a2f9983edcc76760f4ca42e91bf56
Lapololucy <lapololucy@gmail.com>
2022-10-18 08:46:54 +0000

    Task. 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_ptchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchat('r');
+
+	rturn (0);
+}
+
