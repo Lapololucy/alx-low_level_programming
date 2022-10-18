1f0ed98354a125df5aaa211fdf1bb31098fd3898
Lapololucy <lapololucy@gmail.com>
2022-10-18 13:33:10 -0700

    Task.1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - write the lower-case alphabet on the line
+ *
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
