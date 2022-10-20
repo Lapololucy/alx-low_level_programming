ff63ac5ca666eabc460c26fc21bf18d2a74b94d0
Lapololucy <lapololucy@gmail.com>
2022-10-20 03:43:46 -0700

    Task .8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print a square to a given size
+ * @size: size square to print
+ *
+ */
+void print_square(int size)
+{
+	int vert, horiz;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (vert = size ; vert > 0 ; vert--)
+	{
+		for (horiz = size ; horiz > 0; horiz--)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+	}
+}
+
