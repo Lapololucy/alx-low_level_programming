4ea1372276fad9a02986feffb1f89d0220598626
Lapololucy <lapololucy@gmail.com>
2022-10-24 04:31:48 -0700

    Task.5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string -> afunction that reverses a string.
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int start, temp;
+	int end = 0;
+
+	while (*(s + end) != '\0')
+	{
+		end++;
+	}
+
+	end -= 1;
+	start = 0;
+	while (end > start)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+
+}
+
