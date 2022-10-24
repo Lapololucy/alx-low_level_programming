b0187dcb15f052a90da62ce4276335838d09693e
Lapololucy <lapololucy@gmail.com>
2022-10-24 04:08:30 -0700

    Task.4 cor

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,17 @@
 #include "main.h"
 /**
- * print_rev -> a function to print a stdout in reverse
+ * print_rev -> a function to print a string in reverse
  * @s: the value to process to the stdout in reverse
+ * Return: 0
  */
 void print_rev(char *s)
 {
-	int count;
+	int i = 0;
 
-	while (*(s + count) != '\0')
-	{
-		count++;
-	}
-
-	while (count--)
-	{
-		_putchar(*(s + (count)));
-	}
+	while (*(s + i))
+		i++;
+	while (i--)
+		_putchar(*(s + i));
 	_putchar('\n');
 }
 
