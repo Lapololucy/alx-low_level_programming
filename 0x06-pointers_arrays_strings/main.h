4610d30c0ad3dd61b33f57a4ab661ac2fdc3e543
Lapololucy <lapololucy@gmail.com>
2022-10-26 04:58:12 -0700

    Task. 8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,10 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 
 
