07893d3ca6f267af9371922eaa9bbf1984ffbddd
Lapololucy <lapololucy@gmail.com>
2022-10-31 05:16:52 -0700

    Task. 0

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _puychar - write the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
