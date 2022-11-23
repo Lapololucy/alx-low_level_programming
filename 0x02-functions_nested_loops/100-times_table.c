4a12ba9d0ec370194f1cee5511eed5d1e451bfae
Lapololucy <lapololucy@gmail.com>
2022-11-23 05:37:37 -0800

    Task 12 corrected

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,113 +1,46 @@
 #include "main.h"
-
 /**
- * print_thousand - print out the digit in the thousand spot
- * @sum: value to scan for thousand spot
- *
- * Return: void
+ * print_times_table - prints the n times table
+ * @n: the upper limit to be printed
  */
-static void print_thousand(int sum)
+void print_times_table(int n)
 {
-	if (sum / 1000 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		_putchar('0' + (sum / 1000));
-	}
-}
+	int row;
+	int column;
+	int product;
 
-
-/**
- * print_hundred - print out the digit in the hundre spot
- * @sum: value to scan for hundred spot
- *
- * Return: void
- */
-static void print_hundred(int sum)
-{
-	if (sum / 100 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		_putchar('0' + (sum / 100));
-	}
-}
-
-
-/**
- * print_tenth - print out the digit in the the tenth spot
- * @sum: value to scan for tenth spor
- *
- * Return: void
- */
-static void print_tenth(int sum)
-{
-	if (sum / 10 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		if ((sum / 10) > 9)
-		{
-			_putchar('0' + (sum / 10 % 10));
-		}
-		else
-		{
-			_putchar('0' + (sum / 10));
-		}
-	}
-}
-
-
-/**
- * print_base10 - print out the digit in the base10 spot
- * @sum: value to scan for base10 spot
- *
- * Return: void
- */
-static void print_base10(int sum)
-{
-	if (sum % 10 == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		_putchar('0' + (sum % 10));
-	}
-}
-
-/**
- * print_times_table - print out the 9 times table
- * @n: value use to generate times table.
- *
- * Return: void
- */
-void print_time_table(int n)
-{
-	int v, h;
-
-	if (n > 15)
-	{
+	if (n < 0 || n >= 15)
 		return;
-	}
-	for (h = 0 ; h < n + 1 ; h++)
+	for (row = 0; row <= n; row++)
 	{
-		_putchar('0');
-		for (v = 1 ; v < n + 1 ; v++)
+		for (column = 0; column <= n; column++)
 		{
-			int sum = v * h;
-
-			_putchar(',');
-			print_thousand(sum);
-			print_hundred(sum);
-			print_tenth(sum);
-			print_base10(sum);
+			product = (row * column);
+			if (column == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			}
 		}
 		_putchar('\n');
 	}
