321c01bbcae48b59b953adfc137ae82681b4e10e
Lapololucy <lapololuucy@gmail.com>
2023-01-16 02:39:36 -0800

    fixing

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -30,4 +27,4 @@ int mul(int a, int b);
 int mod(int a, int b);
 extern div_t div(int __numer, int __denom);
 
-#endif /* MAIN_H */
+#endif 
